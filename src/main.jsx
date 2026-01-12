a2397ba0c04a632770f23d9e5c4e8a32ceb55f80
Parkhi Mahajan <parkhimahajan16@gmail.com>
2026-01-13 03:23:16 +0530

    Fix script path for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import App from "./App";
+import App from "./App.jsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
+const root = createRoot(document.getElementById("root"));
+
+root.render(
   <HashRouter>
     <App />
   </HashRouter>
